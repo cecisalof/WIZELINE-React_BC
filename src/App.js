85c982b4a99062f4de2796f6417466939512af3c
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2021-07-01 19:26:16 +0200

    Data in parent component Printing data in the console from parent component & passing it to the Child as a prop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
+import { useState } from "react";
 import "./styles.css";
 import Pokemon from "./components/PokeCard";
+import { mockPokemonData } from "./mock/pokeData.js";
 
-export default function App() {
+export default function App(pokemonData) {
+  const [data, setData] = useState({});
+  console.log("data", mockPokemonData);
+
+  const parentToChild = () => {
+    setData(mockPokemonData);
+  };
+
   return (
     <div className="App">
       <h1>WelcomE to your first mini challenge!</h1>
-      <Pokemon />
+      <Pokemon
+        parentToChild={data}
+        // key={data}
+        // setData ={setData}
+      />
     </div>
   );
 }
--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,13 +1,15 @@
 import React from "react";
-import mockPokemonData from "../mock/pokeData.js";
+// import mockPokemonData from "../mock/pokeData.js";
 
-const Pokemon = () => {
-  //const data = mockPokemonData;
-  //const { name, sprites, video } = data;
-  console.log(mockPokemonData);
+const Pokemon = (pokemonData) => {
+  console.log("DATA IN CHILD COMPONENT", pokemonData);
+
   return (
     <>
-      <h1>Pokemon</h1>
+      <h5>Información del pokemon</h5>
+      <h1>{pokemonData.name}</h1>
+      <h1>{pokemonData.sprites}</h1>
+      <h1>{pokemonData.video}</h1>
     </>
   );
 };
