1e0b29921fb2992536f944fa7501207ea87a2d0c
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2021-07-01 18:00:50 +0200

    Function component Function component in PokeCard has been created

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./styles.css";
+import Pokemon from "./components/PokeCard";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Welcome to your first mini challenge!</h1>
+      <h1>WelcomE to your first mini challenge!</h1>
+      <Pokemon />
     </div>
   );
 }
---
+++ src/components/PokeCard.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import mockPokemonData from "../mock/pokeData.js";
+
+const Pokemon = () => {
+  //const data = mockPokemonData;
+  //const { name, sprites, video } = data;
+  console.log(mockPokemonData);
+  return (
+    <>
+      <h1>Pokemon</h1>
+    </>
+  );
+};
+
+export default Pokemon;
