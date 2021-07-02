a08c45e737d9fc557319ea773aec7c63a61bc5f2
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2021-07-02 13:01:38 +0200

    Module Css Adding some Css to PokeCard file

--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,19 +1,28 @@
 import React from "react";
 import { mockPokemonData } from "../mock/pokeData.js";
+import "./PokeCard.css";
 
 const Pokemon = (pokemonData) => {
   console.log("DATA IN CHILD COMPONENT", mockPokemonData);
   // const { name, sprites, video } = pokemonData;
 
   return (
-    <>
+    <div className="card-container">
       <h1>{mockPokemonData.name}</h1>
-      <img src={mockPokemonData.sprites.front_default} alt="" />
-      <img src={mockPokemonData.sprites.front_shiny} alt="" />
+      <img
+        className="card-img"
+        src={mockPokemonData.sprites.front_default}
+        alt=""
+      />
+      <img
+        className="card-img"
+        src={mockPokemonData.sprites.front_shiny}
+        alt=""
+      />
       <a href={mockPokemonData.video}>
-        <button>Watch video here!</button>
+        <button className="card-button">Watch video here!</button>
       </a>
-    </>
+    </div>
   );
 };
 
