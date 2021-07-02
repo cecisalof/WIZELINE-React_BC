f967e3ad95ebbd0e7d841caadb389703d5262e4b
Cecilia Losada <cecilia.sanchezlosada@gmail.com>
2021-07-02 12:27:07 +0200

    Data is shown in the screen Directly loaded in the child component, data is shown in the screen as required in this challenge.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from "react";
-import { useState } from "react";
+//import { useState } from "react";
 import "./styles.css";
 import Pokemon from "./components/PokeCard";
 import { mockPokemonData } from "./mock/pokeData.js";
 
 export default function App() {
-  const [data, setData] = useState({});
+  //const [data, setData] = useState({});
   console.log("data", mockPokemonData);
 
-  const parentToChild = () => {
-    setData(mockPokemonData);
-  };
+  // const parentToChild = () => {
+  //   setData(mockPokemonData);
+  // };
 
   return (
     <div className="App">
-      <h2>Find all pokemon´s info in the card below:</h2>
+      <h2>Pokemon cards</h2>
       <Pokemon
-        parentToChild={data}
-        // key={data}
-        // setData ={setData}
+      //parentToChild={data}
+      // key={data}
+      // setData ={setData}
       />
     </div>
   );
--- src/components/PokeCard.jsx
+++ src/components/PokeCard.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-// import mockPokemonData from "../mock/pokeData.js";
+import { mockPokemonData } from "../mock/pokeData.js";
 
 const Pokemon = (pokemonData) => {
-  //console.log("DATA IN CHILD COMPONENT", pokemonData);
-  const { name, sprites, video } = pokemonData;
+  console.log("DATA IN CHILD COMPONENT", mockPokemonData);
+  // const { name, sprites, video } = pokemonData;
 
   return (
     <>
-      <h1>{name}</h1>
-      <h5>{sprites}</h5>
-      <h4>{video}</h4>
+      <h1>{mockPokemonData.name}</h1>
+      <img src={mockPokemonData.sprites.front_default} alt="" />
+      <img src={mockPokemonData.sprites.front_shiny} alt="" />
+      <a href={mockPokemonData.video}>
+        <button>Watch video here!</button>
+      </a>
     </>
   );
 };
